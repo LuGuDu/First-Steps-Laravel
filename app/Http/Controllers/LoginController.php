e5a712d72d1fb16c3a3db874d20da2d66478c2d5
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-02-09 16:08:44 +0100

    login complete

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $email = request('login');
+        $login = request('login');
         $pass = request('password');
 
-        
+        $user = User::all()
+             ->where('name', '==', $login);
 
-        return $email . ' -> ' . $pass;
+        if(count($user) == 0){
+            return redirect()->back()->with('error', 'Nombre o contraseña incorrectos');   
+        }  
+        $password = $user[0]["password"];
+
+        if($password == $pass)
+        {
+            return view('Welcome');
+        }
+        else {
+            return redirect()->back()->with('error', 'Nombre o contraseña incorrectos');   
+        }
+
     }
 }
